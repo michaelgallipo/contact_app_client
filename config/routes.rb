dd9b59db07cefb419f63d29aab29a85188733235
michael <michael.gallipo@gmail.com>
2018-06-13 01:00:38 -0400

    Added user accounts, login/logout, linked contacts to one user

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,12 @@ Rails.application.routes.draw do
     patch "contacts/:id" => "contacts#update"
     delete "contacts/:id" =>  "contacts#destroy"
   end
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+  
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
 end
